eb6800aae9a788d4406c950b8def83673e797b70
Alex Clavelle <aclavelle@envoc.com>
2015-07-23 09:47:21 -0500

    user signup

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,6 +5,26 @@ module.exports = {
     //set user on session: {userName:userName,signedIn:true}
     //save session
     //send json to client: {user:req.session.user}
+    var params = req.params.all();
+    var userName = params.username ? params.username.trim() : "";
+
+    if (userName && userName != "") {
+      req.session.user = {
+        userName:userName,
+        signedIn:true
+      };
+      return req.session.save(function(){
+        return res.ok({
+          message: 'username set to: ' + userName,
+          user:req.session.user
+        });
+      });
+    } else {
+      res.status(400);
+      res.json({
+        message: 'please specify a username'
+      });
+    }
   },
   get:function(req,res) {
     var session = req.session;
