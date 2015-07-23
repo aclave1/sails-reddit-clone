08f57742b4450711c881df437ed70139e23c62f1
Alex Clavelle <aclavelle@envoc.com>
2015-07-23 09:45:27 -0500

    Starting point

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -1,36 +1,12 @@
 module.exports = {
   getComments: function (req, res) {
-    var params = req.params.all();
-    return Comment
-      .find({
-        post: params.post
-      })
-      .then(function (comments) {
-        res.json({payload: comments});
-      });
+    //find comments by params.post
+    //send response: payload:comments
   },
   createComment: function (req, res) {
-    var params = req.params.all();
-    var userName = !_.isUndefined(req.session.user) ? req.session.user.userName : "Anonymous";
-    return Comment
-      .create({
-        parent: params.parent,
-        contents: params.contents,
-        post: params.post,
-        userName:userName
-      })
-      .then(function (created) {
-        var postId = created.post;
-        var response = {payload:created};
-        notifyPostRoom(req,postId,response);
-        res.ok(response);
-      })
-      .catch(function(error){
-        res.json({
-          errors:[error],
-          hasErrors:true
-        });
-      });
+    //get username from session || "anonymous"
+    //create comment:{userName, params.parent,post,contents,}
+    //notifyPost room, res.ok with {payload:created}
   }
 };
 
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,25 +1,10 @@
 module.exports = {
   signUp: function (req, res) {
-    var params = req.params.all();
-    var userName = params.username ? params.username.trim() : "";
-
-    if (userName && userName != "") {
-      req.session.user = {
-        userName:userName,
-        signedIn:true
-      };
-      return req.session.save(function(){
-        return res.ok({
-          message: 'username set to: ' + userName,
-          user:req.session.user
-        });
-      });
-    } else {
-      res.status(400);
-      res.json({
-        message: 'please specify a username'
-      });
-    }
+    //extract username from request parameters
+    //handle errors
+    //set user on session: {userName:userName,signedIn:true}
+    //save session
+    //send json to client: {user:req.session.user}
   },
   get:function(req,res) {
     var session = req.session;
