b2cf7bbc5dce82caae13c895efb80472272b79ab
Alex Clavelle <aclavelle@envoc.com>
2015-07-23 10:03:40 -0500

    create comment

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -7,6 +7,27 @@ module.exports = {
     //get username from session || "anonymous"
     //create comment:{userName, params.parent,post,contents,}
     //notifyPost room, res.ok with {payload:created}
+    var params = req.params.all();
+    var userName = !_.isUndefined(req.session.user) ? req.session.user.userName : "Anonymous";
+    return Comment
+      .create({
+        parent: params.parent,
+        contents: params.contents,
+        post: params.post,
+        userName:userName
+      })
+      .then(function (created) {
+        var postId = created.post;
+        var response = {payload:created};
+        notifyPostRoom(req,postId,response);
+        res.ok(response);
+      })
+      .catch(function(error){
+        res.json({
+          errors:[error],
+          hasErrors:true
+        });
+      });
   }
 };
 
